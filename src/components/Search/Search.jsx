72edeca1b1f8047270e4b838ed593b92bf5bc713
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-30 13:56:21 -0600

    agregando w-full a los contenedores del search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,11 @@
 const Search = () => {
 
   return (
-    <div className="">
-      <form className="">
-        <div>
-          <input className="mr-2 border rounded-md p-1" type="text" placeholder="Search" />
-          <button className="border rounded-md p-1 bg-white" type="submit">Search</button>
+    <div className="w-full">
+      <form className="w-full">
+        <div className="w-full flex">
+          <input className="mr-2 border rounded-md p-1 flex-grow " type="text" placeholder="Search" />
+          <button className="border rounded-md p-1 bg-white flex-grow-0" type="submit">Search</button>
         </div>
       </form>
     </div>
