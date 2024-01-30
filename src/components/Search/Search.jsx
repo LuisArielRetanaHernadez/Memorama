44208bb7a1f37df666efee1521c9d4710d59e6a8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-30 13:53:37 -0600

    agregando estilos al input y el butrton

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,11 @@
 const Search = () => {
 
   return (
-    <div>
-      <form>
+    <div className="">
+      <form className="">
         <div>
-          <input type="text" placeholder="Search" />
-          <button type="submit">Search</button>
+          <input className="mr-2 border rounded-md p-1" type="text" placeholder="Search" />
+          <button className="border rounded-md p-1 bg-white" type="submit">Search</button>
         </div>
       </form>
     </div>
