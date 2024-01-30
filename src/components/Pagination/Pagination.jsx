49e97f57cae9d2e415bc2cdd49e2b524963c1663
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-30 16:38:04 -0600

    agregadno padding a los indexes de la paginacion

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom"
 const Pagination = ({total, amountPages}) => {
 
   return (
-    <div className=" border border-blue-950 rounded-md bg-slate-100 flex items-center">
+    <div className="mt-2 w-auto border border-blue-950 rounded-md bg-slate-100 inline-flex items-center">
       <div>
-        <Link>Prev</Link>
+        <Link className="rounded-md px-2 py-2 inline-flex">
+          Prev
+        </Link>
       </div>
       <div className="flex">
-        <Link className="bg-blue-950 text-white rounded-md px-3">1</Link>
-        <Link className="bg-blue-950 text-white rounded-md px-3">2</Link>
-        <Link className="bg-blue-950 text-white rounded-md px-3">3</Link>
+        <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex">1</Link>
+        <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex">2</Link>
+        <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex">3</Link>
       </div>
       <div>
-        <Link>Next</Link>
+        <Link className="rounded-md px-2 py-2 inline-flex">
+          Next
+        </Link>
       </div>
     </div>
   )
