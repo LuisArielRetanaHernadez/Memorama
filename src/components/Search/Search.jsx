500072742edb93c977ac354ab9b4cbb6c0c3befb
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-30 13:42:13 -0600

    creando el componente 'Search'

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,15 @@
+const Search = () => {
+
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="Search" />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Search
