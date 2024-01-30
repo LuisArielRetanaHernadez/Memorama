da9241428d87efbaeb8cea687a03f5d1506a087b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-30 17:00:10 -0600

    sincronizando le estado del componente con la url

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 
 const Pagination = ({total, amountPages}) => {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  let location = useLocation()
+
+  useEffect(() => {
+    const numberPage = parseInt(location.search.split('=')[1])
+    if(numberPage) setCurrentPage(numberPage)
+  },[location])
 
   return (
     <div className="mt-2 w-auto border border-blue-950 rounded-md bg-slate-100 inline-flex items-center">
@@ -10,7 +19,9 @@ const Pagination = ({total, amountPages}) => {
         </Link>
       </div>
       <div className="flex">
-        <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex">1</Link>
+        <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex"
+          to={`/?page=${currentPage}`}
+          >1</Link>
         <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex">2</Link>
         <Link className="text-white px-3 py-2 border-x border-x-slate-500 bg-blue-950 inline-flex">3</Link>
       </div>
