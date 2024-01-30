3e2fbb1720260d865e736a9fe728d106c98dbd1e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-30 14:10:30 -0600

    quitando el submit al button

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,8 +4,8 @@ const Search = () => {
     <div className="w-full">
       <form className="w-full">
         <div className="w-full flex">
-          <input className="mr-2 border rounded-md p-1 flex-grow " type="text" placeholder="Search" />
-          <button className="border rounded-md p-1 bg-white flex-grow-0" type="submit">Search</button>
+          <input className="mr-2 border rounded-md p-1 outline-none flex-grow " type="text" placeholder="Search" />
+          <button className="border rounded-md p-1 bg-white flex-grow-0">Search</button>
         </div>
       </form>
     </div>
