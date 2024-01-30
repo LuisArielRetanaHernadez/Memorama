a888b6fe3eca62573134a53b3f115c4c05d619b0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-29 21:42:24 -0600

    creando el array 'room'

---
+++ src/utils/api.js
@@ -0,0 +1,52 @@
+export const rooms = [
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Single Room',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Sala FF',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Vamos  jugar',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Sala f',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Recuerdas',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'A ver',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Sala Good',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'The good',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Solo IQ',
+    isPrivate: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Single Roo',
+    isPrivate: false,
+  },
+]
