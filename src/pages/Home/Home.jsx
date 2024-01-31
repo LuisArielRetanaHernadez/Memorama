07239dc3b1ac1b7e28c121cf7e76a830a059a7c5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-31 15:56:01 -0600

    agregando los estados indexStart y indexEnd y la funcion updateIndex

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,13 @@ import Pagination from "../../components/Pagination/Pagination"
 
 const Home = () => {
   const [salas, setSalas] = useState(rooms)
+  const [indexStart, setIndexStart] = useState(0)
+  const [indexEnd, setIndexEnd] = useState(undefined)
+
+  const updateIndex = (start, end) => {
+    setIndexStart(start)
+    setIndexEnd(end)
+  }
 
   return (
     <section className="mt-2">
@@ -23,10 +30,10 @@ const Home = () => {
             id={sala.id}
             title={sala.title}
           />
-        ))}
+        )).slice(indexStart, indexEnd)}
       </div>
       <div className="flex justify-center">
-        <Pagination total={salas.length} numbersPages={4} totalSource={6} />
+        <Pagination total={salas.length} numbersPages={4} totalSource={6} updateIndex={updateIndex} />
       </div>
     </section>
   )
