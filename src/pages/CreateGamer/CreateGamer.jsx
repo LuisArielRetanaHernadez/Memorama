da79d80f79f731deac459ca407c446d1c109f3c1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-01 15:03:52 -0600

    agregando estilos al input del titulo

--- src/pages/CreateGamer/CreateGamer.jsx
+++ src/pages/CreateGamer/CreateGamer.jsx
@@ -3,8 +3,8 @@ const CreateGamer = () => {
   return (
     <div className="bg-blue-700 grid grid-cols-create-gamer grid-rows-create-gamer justify-center">
       <form className="p-2 rounded-md bg-slate-300">
-        <div>
-          <input placeholder="Titulo"/>
+        <div className="">
+          <input className="w-full font-semibold text-center border-b border-b-slate-950 p-1 outline-none bg-slate-300" placeholder="Titulo"/>
         </div>
         <div>
           <input type="number" placeholder="N. Jugadores" max={3}/>
