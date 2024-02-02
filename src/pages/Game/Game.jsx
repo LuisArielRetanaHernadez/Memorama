9f1877b0660d8b39e7808c69cb617bd96335fb44
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-01 20:50:33 -0600

    agregando un div envolvente al titulo, tiempo turcnww

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -2,17 +2,18 @@ const Game = () => {
 
   return (
     <section className="bg-blue-400 min-h-wrapped-menu flex">
-    <div>
-      <div className="bg-blue-900 h-wrapped-menu">
-      <h2>Title</h2>
-      <span>50 segundos</span>
-      <h3>Jugadores</h3>
+    <div className="w-[15%] h-wrapped-menu">
+      <div>
+        <h2>Title</h2>
+        <span>50 segundos</span>
+        <h3>Jugadores</h3>
+      </div>
+      
         <ul>
           <li>Jugador 1</li>
           <li>Jugador 2</li>
           <li>Jugador 3</li>
         </ul>
-      </div>
     </div>
     <div className="bg-green-600 flex-grow">
         {/*targets of gamer */}
