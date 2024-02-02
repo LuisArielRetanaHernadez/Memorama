c46e795e88ef81b60ca99f6b043832dd119656e4
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-01 20:53:17 -0600

    agregando un div envolvente a la lista de jugadores del juego

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -6,14 +6,17 @@ const Game = () => {
       <div>
         <h2>Title</h2>
         <span>50 segundos</span>
-        <h3>Jugadores</h3>
       </div>
-      
+
+      <div>
+        <h3>Jugadores</h3>
         <ul>
           <li>Jugador 1</li>
           <li>Jugador 2</li>
           <li>Jugador 3</li>
         </ul>
+      </div>
+
     </div>
     <div className="bg-green-600 flex-grow">
         {/*targets of gamer */}
