0b99544c54543dc4373796f02fa24082cb90950f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-01 20:18:01 -0600

    creando el poge 'Game'

---
+++ src/pages/Game/Game.jsx
@@ -0,0 +1,25 @@
+const Game = () => {
+
+  return (
+    <section>
+    <div>
+      <h2>Title</h2>
+      <span>50 segundos</span>
+      <div>
+      <h3>Jugadores</h3>
+        <ul>
+          <li>Jugador 1</li>
+          <li>Jugador 2</li>
+          <li>Jugador 3</li>
+        </ul>
+      </div>
+      <div>
+        {/*targets of gamer */}
+      </div>
+    </div>
+
+    </section>
+  )
+}
+
+export default Game;
