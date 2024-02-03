8ab35f0abb40791fc106249948d095edd0326d4a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-03 17:25:49 -0600

    agrgando estilos al contenedor de la seccion de jugador

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -12,13 +12,13 @@ const Game = () => {
       </div>
 
       <div>
-        <div className="flex justify-center items-center">
+        <div className="mb-4 flex justify-center items-center">
           <div className="text-blue-500 p-2 rounded-xl bg-gray-200 inline-flex justify-center items-center">
             <FontAwesomeIcon icon={faUsers} className="mr-2" />
             <h3 className="font-semibold">Jugadores</h3>
           </div>
         </div>
-        <ul>
+        <ul className="flex flex-col gap-2">
           <li>Jugador 1</li>
           <li>Jugador 2</li>
           <li>Jugador 3</li>
