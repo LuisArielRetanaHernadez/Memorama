2e32195f2b0023cf8de918641453dc0a15ec94ca
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-03 18:57:57 -0600

    creando el componente Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+const Card = ({content}) => {
+
+  return (
+    <div>
+      <div>
+        <p>
+          {content}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
