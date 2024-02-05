b5742bc27e3eb166fa4f5239b7bfb7d0160df57d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-05 14:08:21 -0600

    elimando las clase rounded-md, shadow-2xl, shadow-black y bg-white para despues agregarselo a los dos div hijo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,13 @@
 const Card = ({content}) => {
 
   return (
-    <div className="rounded-md shadow-2xl shadow-black bg-white relative card-flip">
-      <div className="w-full h-full flex justify-center items-center absolute backface-hidden rotate-x-180 ease-in back">
+    <div className="rounded-md relative card-flip">
+      <div className="w-full h-full rounded-md shadow-2xl shadow-black bg-white  flex justify-center items-center absolute backface-hidden rotate-x-180 ease-in back">
         <p className="font-semibold text-lg text-blue-900">
           {content}
         </p>
       </div>
-      <div className="w-full h-full flex justify-center items-center absolute backface-hidden rotate-y-0 ease-in front">
+      <div className="w-full h-full rounded-md shadow-2xl shadow-black bg-white  flex justify-center items-center absolute backface-hidden rotate-y-0 ease-in front">
         <div>
           ¿Lo recuerdas?...
         </div>
