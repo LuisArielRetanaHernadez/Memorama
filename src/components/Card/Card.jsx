b1151d9e34eb0db6eff5a77b13b8e4a99d8c57ea
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-05 13:39:15 -0600

    agregando las clases backface-hidden a los dos div hijo y agregando las clase rotate-x-180, hover-rotate-x-180

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,13 @@
 const Card = ({content}) => {
 
   return (
-    <div className="rounded-md shadow-2xl shadow-black bg-white relative">
-      <div className="w-full h-full flex justify-center items-center absolute">
+    <div className="rounded-md shadow-2xl shadow-black bg-white relative hover-rotate-x-180">
+      <div className="w-full h-full flex justify-center items-center absolute backface-hidden rotate-x-180">
         <p className="font-semibold text-lg text-blue-900">
           {content}
         </p>
       </div>
-      <div className="w-full h-full flex justify-center items-center absolute">
+      <div className="w-full h-full flex justify-center items-center absolute backface-hidden">
         <div>
           ¿Lo recuerdas?...
         </div>
