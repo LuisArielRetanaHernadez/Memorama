7cddad4fa4ed42776e0a783fb4047b6b65682154
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-05 14:13:25 -0600

    creando el estado select para confirmar si la tarjeta encontro su par

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react'
+
 const Card = ({content}) => {
+  const [select, setSelect] = useState(false);
 
   return (
     <div className="rounded-md relative card-flip">
