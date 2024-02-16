4ab774a1f8e9d673186b3480497ed5e9e319baf5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-16 17:51:39 -0600

    creando el service getGames para obtener los games en paginacion

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,5 +1,11 @@
 import { instanceAxios } from "./Axios"
 
+export const getGames = (limit, page) => {
+  return instanceAxios.get(`/games?limit=${limit}&page=${page}`)
+    .then(res => res.data)
+    .catch(err => err)
+}
+
 export const createGame = (config) => {
   return instanceAxios.post('/game', config)
     .then(res => res.data)
